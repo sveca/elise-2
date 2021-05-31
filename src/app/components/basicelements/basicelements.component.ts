eaef27e1bdca39cff8779a1ce5c5a3ffea44f6d0
Aida Monfort Muriach <aida.monfort@uji.es>
2021-05-31 18:55:58 +0200

    NUTS regions dropdown filters

--- src/app/components/basicelements/basicelements.component.ts
+++ src/app/components/basicelements/basicelements.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, ChangeDetectorRef, asNativeElements } from '@angular
 import { icon, latLng, Layer, marker, tileLayer } from 'leaflet';
 import { createAsExpression } from 'typescript';
 import cases from '../../../assets/cases.json';
+import nutsJSON from '../../../assets/nuts-labels.json';
 
 @Component({
   selector: 'app-basicelements',
@@ -29,12 +30,29 @@ export class BasicelementsComponent implements OnInit {
     center: latLng(46.879966, 0.726909)
   };
 
+  nuts = nutsJSON;
+  nuts0Labels = [];
+  nuts2Labels = [];
+  nuts3Labels = [];
 
   markers: Layer[] = [];
 
   filteredCases = cases;
 
   selectedCase = null;
+
+  thematicAreas = [
+    { name: '01 - General public services', number: 1, active: false },
+    { name: '02 - Defence', number: 2, active: false },
+    { name: '03 - Public order and safety', number: 3, active: false },
+    { name: '04 - Economic affairs', number: 4, active: false },
+    { name: '05 - Environmental protection', number: 5, active: false },
+    { name: '06 - Housing and community amenities', number: 6, active: false },
+    { name: '07 - Health', number: 7, active: false },
+    { name: '08 - Recreation, culture and religion', number: 8, active: false },
+    { name: '09 - Education', number: 9, active: false },
+    { name: '10 - Social protection', number: 10, active: false },
+  ];
 
   addMarker(ca) {
     const newMarker = marker(
@@ -75,6 +93,22 @@ export class BasicelementsComponent implements OnInit {
     this.filteredCases.forEach(c => {
       this.addMarker(c);
     })
+
+    this.nuts.forEach(n => {
+      // console.log(n.NUTS_ID);
+      if (n.NUTS_ID.length === 2) { // NUTS 0
+        this.nuts0Labels.push({ NUTS_ID: n.NUTS_ID, CNTR_CODE: n.CNTR_CODE, NAME_LATN: n.NAME_LATN, NUTS_NAME: n.NUTS_NAME, active: false })
+      } else if (n.NUTS_ID.length === 4) { // NUTS 2
+        this.nuts2Labels.push({ NUTS_ID: n.NUTS_ID, CNTR_CODE: n.CNTR_CODE, NAME_LATN: n.NAME_LATN, NUTS_NAME: n.NUTS_NAME, active: false })
+      } else if (n.NUTS_ID.length > 4) { // NUTS 3
+        this.nuts3Labels.push({ NUTS_ID: n.NUTS_ID, CNTR_CODE: n.CNTR_CODE, NAME_LATN: n.NAME_LATN, NUTS_NAME: n.NUTS_NAME, active: false })
+      }
+    });
+
+    /*     this.nuts.features.forEach(n => {
+          console.log(n.properties.FID);
+        }); */
+
 
   }
 
