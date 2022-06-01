783f8490ff15dfd64ac4a77ac8fe60cfe8e0298b
sveca <sven.casteleyn@uji.es>
2022-06-01 15:36:18 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  base_url: 'https://geotecinit.github.io/elise/#/home',
+  base_url: 'https://github.com/sveca/elise-2/#/home',
   cases_json_url: 'https://raw.githubusercontent.com/GeoTecINIT/elise/main/src/assets/cases.json'
 };
