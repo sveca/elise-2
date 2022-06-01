d5bb2c964b4aeb03f3b57649ec84c233520c9505
sveca <sven.casteleyn@uji.es>
2022-06-01 15:39:59 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,5 +8,5 @@
 export const environment = {
   production: false,
   base_url: 'https://github.com/sveca/elise-2/#/home',
-  cases_json_url: 'https://raw.githubusercontent.com/GeoTecINIT/elise/main/src/assets/cases.json'
+  cases_json_url: 'https://raw.githubusercontent.com/sveca/elise-2/main/src/assets/cases.json'
 };
